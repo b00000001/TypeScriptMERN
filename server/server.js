e9b15867c9d81804ce6cf98b329a4b26e726a707
Aaron Deas <deas.aaron@gmail.com>
2021-09-19 18:47:59 -0400

    Pushing small changes made to server.js to adhere to Lint and Typescript standards

--- server/server.js
+++ server/server.js
@@ -1,6 +1,12 @@
-var express = require('express');
-var app = express();
+"use strict";
+exports.__esModule = true;
+var express_1 = require("express");
+var app = (0, express_1["default"])();
 var port = process.env.PORT || 3000;
-app.listen(port, function (req, res) {
-    res.end('Hello');
+app.get('/', function (req, res) {
+    res.send('Hello World!');
+});
+app.listen(port, function () {
+    //eslint-disable-next-line no-console
+    console.log("App running at http://localhost:" + port);
 });
--- server/server.ts
+++ server/server.ts
@@ -1,8 +1,10 @@
-const express = require('express');
+import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
 app.listen(port, () => {
     //eslint-disable-next-line no-console
