193f2cc0134dc03026d662ce416cd1c6f636d13c
Aaron Deas <deas.aaron@gmail.com>
2021-09-19 15:34:57 -0400

    Further setup ESLint, built framework for express portion of app

--- server/server.ts
+++ server/server.ts
@@ -1,1 +1,10 @@
-console.log('Hello');
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+
+
+app.listen(port, () => {
+    //eslint-disable-next-line no-console
+    console.log(`App running at http://localhost:${port}`)
+});
