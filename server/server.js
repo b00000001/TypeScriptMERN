64ecd0896a798d521e8d47c6ca329f9d5efaabd2
Aaron Deas <deas.aaron@gmail.com>
2021-09-20 17:56:53 -0400

    Added apolloserver for mysql, setup connection.js for server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,12 @@
-var express = require('express');
+var express = require("express");
 var app = express();
+require("dotenv").config();
+const { ApolloServer } = require("apollo-server-express");
 var port = process.env.PORT || 3001;
-app.get('/', function (req, res) {
-    res.send('Hello World!');
+app.get("/", function (req, res) {
+  res.send("Hello World!");
 });
 app.listen(port, function () {
-    //eslint-disable-next-line no-console
-    console.log("App running at http://localhost:" + port);
+  //eslint-disable-next-line no-console
+  console.log("App running at http://localhost:" + port);
 });
