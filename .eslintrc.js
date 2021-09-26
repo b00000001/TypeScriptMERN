5a5897774109af2b173baf5e9de5d5adcc88264a
Aaron Deas <deas.aaron@gmail.com>
2021-09-26 19:54:06 -0400

    Added Eslint options

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,7 @@ module.exports = {
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
-    'plugin:@typescript-eslint/recommended-requiring-type-checking'
+    'plugin:@typescript-eslint/eslint-recommended'
   ],
   rules: {
     'no-console': ['error']
