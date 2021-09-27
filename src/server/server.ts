2f5c812bdc7097def3c6a72470564b5bc0de33dc
Aaron Deas <deas.aaron@gmail.com>
2021-09-26 21:31:54 -0400

    Finalized setup for eslint and typescript for both client and server directories

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,1 +1,25 @@
-console.log('hello');
+import express from 'express';
+import * as dotenv from 'dotenv';
+import * as path from 'path';
+dotenv.config();
+
+
+const app = express();
+
+const PORT = process.env.PORT || 3001;
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '../client/build')));
+  }
+  
+  app.get('*', (_, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
+
+app.listen(PORT,() => {
+    //eslint-disable-next-line no-console
+    console.log(`Server listening on port ${PORT}`);
+})
