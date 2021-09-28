20678f272c3c870f65ed645263dad8883acf04cd
Aaron Deas <deas.aaron@gmail.com>
2021-09-27 20:44:22 -0400

    Added unary operators to connection string for db in server/config/connection.ts

--- src/server/server.ts
+++ src/server/server.ts
@@ -7,7 +7,7 @@ dotenv.config();
 
 const app = express();
 const server = new ApolloServer({
-    typeDefs
+  typeDefs
 })
 
 const PORT = process.env.PORT || 3001;
@@ -16,14 +16,13 @@ app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
-  }
-  
-  app.get('*', (_, res) => {
-    res.sendFile(path.join(__dirname, '../client/build/index.html'));
-  });
+  app.use(express.static(path.join(__dirname, '../client/build')))
+}
+app.get('*', (_, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+})
 
-app.listen(PORT,() => {
-    //eslint-disable-next-line no-console
-    console.log(`Server listening on port ${PORT}`);
-});
+app.listen(PORT, () => {
+// eslint-disable-next-line no-console
+  console.log(`Server listening on port ${PORT}`)
+})
