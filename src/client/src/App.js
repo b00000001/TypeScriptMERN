b50541be61758ac6a36fb485b314bdaa4bf71a52
Aaron Deas <deas.aaron@gmail.com>
2021-09-30 00:01:33 -0400

    completed setup for react router, added header page, installed @types/react-router-dom

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,30 +1,8 @@
-/* eslint-disable import/first */
-
-var __assign =
-  (this && this.__assign) ||
-  function () {
-    __assign =
-      Object.assign ||
-      function (t) {
-        for (var s, i = 1, n = arguments.length; i < n; i++) {
-          s = arguments[i];
-          for (var p in s)
-            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
-        }
-        return t;
-      };
-    return __assign.apply(this, arguments);
-  };
-import { jsx as _jsx } from 'react/jsx-runtime';
+import { jsx as _jsx } from "react/jsx-runtime";
 import './App.css';
+import { BrowserRouter as Router } from 'react-router-dom';
+import Header from '../src/pages/Header';
 function App() {
-  return _jsx(
-    'div',
-    __assign(
-      { className: 'App' },
-      { children: _jsx('h1', { children: 'Welcome' }, void 0) }
-    ),
-    void 0
-  );
+    return (_jsx(Router, { children: _jsx("div", { children: _jsx(Header, {}, void 0) }, void 0) }, void 0));
 }
 export default App;
