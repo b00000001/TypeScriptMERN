3388c3de8317205d113bacc7f47422fc0ee4f4b6
Aaron Deas <deas.aaron@gmail.com>
2021-09-29 23:23:52 -0400

    Added header and footer files, organized file structure in client folder better. Installed @types/react-dom

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
+import Header from '../src/pages/Header';
 function App() {
   return (
-    <div className="App">
-      <h1>Welcome</h1>
-    </div>
+    <Router>
+      <div>
+        <Header />
+      </div>
+    </Router>
   );
 }
 
