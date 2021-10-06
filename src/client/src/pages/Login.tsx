418d325bfce92f92e956d52007bedcc43021eb6e
Aaron Deas <deas.aaron@gmail.com>
2021-10-06 18:24:59 -0400

    Added google login to app, db functionality not connected yet

--- src/client/src/pages/Login.tsx
+++ src/client/src/pages/Login.tsx
@@ -1,12 +1,38 @@
+/* eslint-disable import/first */
+
 import React from 'react';
 import { GoogleLogin } from 'react-google-login';
 import env from 'react-dotenv';
 
 const Login = () => {
-  console.log(env.DB_USER);
+  // const [login, { error }] = useMutation(ADD_GOOGLE_USER);
+  const onSuccess = async (res: any) => {
+    // try {
+    //   const mutationResponse = await login({
+    //     variables: {
+    //       username: res.profileObj.givenName,
+    //       email: res.profileObj.email
+    //     }
+    //   });
+    // } catch (e) {
+    //   console.log(e);
+    // }
+    // window.location.assign("/home");
+    console.log(res);
+  };
+
+  const onFailure = () => {
+    console.log('[Login Failure]');
+  };
   return (
     <div>
-      <h1>Login Page</h1>
+      <GoogleLogin
+        clientId={env.CLIENT_ID}
+        buttonText="Login"
+        onSuccess={onSuccess}
+        onFailure={onFailure}
+        cookiePolicy={'single_host_origin'}
+      />
     </div>
   );
 };
