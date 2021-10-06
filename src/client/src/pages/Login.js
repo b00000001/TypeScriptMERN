d614bb7fc410a70b951f2a469195e4ce654da950
Aaron Deas <deas.aaron@gmail.com>
2021-10-06 18:08:48 -0400

    Added react-dotenv to project to protect potentially sensitive data going forward

--- src/client/src/pages/Login.js
+++ src/client/src/pages/Login.js
@@ -1,5 +1,7 @@
 import { jsx as _jsx } from "react/jsx-runtime";
+import env from 'react-dotenv';
 var Login = function () {
+    console.log(env.DB_USER);
     return (_jsx("div", { children: _jsx("h1", { children: "Login Page" }, void 0) }, void 0));
 };
 export default Login;
--- src/client/src/pages/Login.tsx
+++ src/client/src/pages/Login.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { GoogleLogin } from 'react-google-login';
+import env from 'react-dotenv';
 
 const Login = () => {
+  console.log(env.DB_USER);
   return (
     <div>
       <h1>Login Page</h1>
